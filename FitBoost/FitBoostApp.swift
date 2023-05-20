5991a48cddc6f88b75401781a43018152b00bcdb
dbarszczu <dbarszczu@gmail.com>
2023-05-20 12:02:47 +0200

    Initial Commit

---
+++ FitBoost/FitBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBoostApp.swift
+//  FitBoost
+//
+//  Created by Dominik Barszcz on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
